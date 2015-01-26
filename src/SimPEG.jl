4ae241805d550bdb75abf71ba4da5cfce1feabb5
Rowan Cockett <rowan@3ptscience.com>
2015-01-25 18:58:22 -0800

    plotImage

--- src/SimPEG.jl
+++ src/SimPEG.jl
@@ -5,4 +5,15 @@ include("Mesh.jl")
 include("MeshGrid.jl")
 include("LinearOperators.jl")
 
+using PyCall
+
+export plotImage
+function plotImage(M::SimPEG.AbstractTensorMesh, vec)
+    @pyimport SimPEG as pySimPEG
+    Mesh = PyObject(pySimPEG.Mesh.o)
+    h = M.cnt.dim == 3? (M.hx, M.hy, M.hz): M.cnt.dim == 2? (M.hx, M.hy) : (M.hx,)
+    pyM = pycall(PyObject(Mesh["TensorMesh"]), PyObject, h)
+    pycall(pyM["plotImage"], PyObject, vec)
+end
+
 end
